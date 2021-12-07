1b69c69eca46893ab501b01da6e7d7ce045f80b8
jaguitart <jaguitart@uc.cl>
2021-12-07 15:26:55 -0300

    add images in progress

--- frontend/src/components/AddBird/index.js
+++ frontend/src/components/AddBird/index.js
@@ -1,20 +1,44 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { addBirdImages } from "../../store/imagesReducer";
 
 const AddBird = () => {
+  const history = useHistory();
+  const dispatch = useDispatch()
   const [imageTitle, setImageTitle] = useState('');
   const [imageUrl, setImageUrl] = useState('');
-  const [locationId, setLocatioId] = useState('');
+  const [locationId, setLocatioId] = useState(1);
   const [imageBody, setImageBody] = useState('');
   const [userId, setUserId] = useState('')
   const [albumId, setAlbumId] = useState('');
   const [errors, setErrors] = useState([]);
-  const history = useHistory();
 
+
+  //no llega a conectar con el backend
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newBird = {
+      imageTitle,
+      imageUrl,
+      locationId,
+      imageBody,
+      userId,
+      albumId,
+    };
+
+    let createdBird;
+    console.log(newBird);
+    if (createdBird) {
+      history.push(`/images/${createdBird.id}`);
+    }
+    return dispatch(addBirdImages(newBird))
+  };
 
   return (
     <div className=''>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2 >Add A Bird from ^Country^</h2>
         <label htmlFor='imageUrl' className=''>Bird Url</label>
         <input
@@ -29,11 +53,6 @@ const AddBird = () => {
           placeholder='Bird Name'
         />
         <label htmlFor='imageBody' className=''>Bird comments</label>
-        <input
-          onChange={(e) => setImageBody(e.target.value)}
-          value={imageBody}
-          placeholder='Bird Comments'
-        />
         <input
           onChange={(e) => setImageBody(e.target.value)}
           value={imageBody}
--- frontend/src/store/imagesReducer.js
+++ frontend/src/store/imagesReducer.js
@@ -1,5 +1,7 @@
+import { csrfFetch } from './csrf';
+
 const LOAD_IMAGES = 'images/LOAD_IMAGES'
-const ADD_IMAGE = 'images/ADD_IMAGES'
+const ADD_IMAGE = 'images/ADD_IMAGE'
 // const EDIT_IMAGE = 'images/EDIT_IMAGES'
 // const REMOVE_IMAGE = 'images/REMOVE_IMAGES'
 
@@ -10,10 +12,10 @@ const loadImages = (images) => ({
   images,
 });
 
-// const loadImages = (images) => ({
-//   type: LOAD_IMAGES,
-//   images,
-// });
+const addImage = (images) => ({
+  type: ADD_IMAGE,
+  images,
+});
 
 // SELECTORS
 export const getAllImages = () => async (dispatch) => {
@@ -22,8 +24,22 @@ export const getAllImages = () => async (dispatch) => {
   dispatch(loadImages(images));
 }
 
+export const addBirdImages = (newBird) => async (dispatch) => {
+  const res = await csrfFetch('/api/images/add', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newBird)
+  })
+  if (res.ok) {
+    const bird = await res.json();
+    console.log('*********', bird);
+    dispatch(addImage(bird));
+    return res;
+  }
+}
+
 //INITIAL STATE
-const initialState = { };
+const initialState = {};
 
 // REDUCER
 const imagesReducer = (state = initialState, action) => {
@@ -32,6 +48,13 @@ const imagesReducer = (state = initialState, action) => {
     case LOAD_IMAGES:
       newState = { ...state }
       action.images.forEach(image => { newState[image.id] = image })
+      return newState
+    case ADD_IMAGE:
+      //revisar
+      newState = {
+        ...state,
+        [action.image.id]: action.image
+      };
       return newState
     default:
       return state;
